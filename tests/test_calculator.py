b19a8ad73ffef67ac021492e17c4e1b7aafb0576
Towardsyou <Towardsyou@users.noreply.github.com>
2026-07-04 15:19:38 +0200

    fixup!

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -27,5 +27,9 @@ def test_divide_rejects_zero_divisor():
 def test_is_even_identifies_even_numbers(number, expected):
     assert is_even(number) is expected
 
+
 def test_multiply_returns_product_of_two_numbers():
     assert multiply(2, 3) == 6
+    assert multiply(0, 5) == 0
+    assert multiply(5, 0) == 0
+    assert multiply(0, 0) == 0
